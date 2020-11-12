9fd4ad71ea481012844fda68f21da2f60b83990c
Sanya <purvi.sanya@gmail.com>
2020-11-12 11:54:08 +0400

    merge chat functionality

--- index.js
+++ index.js
@@ -6,13 +6,42 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+const http = require("http").Server(app)
+const socketio = require("socket.io");
 
 // add this to .env
 const PORT = process.env.PORT || 9000
+const SOCKET_PORT = process.env.SOCKET_PORT || 3333
+const io = socketio(http);
+
+
+const Chat = require("./models/chat");
+
+
+io.on("connection", (socket) => {
+    console.log("user connected")
+    socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+
+        chatMsg = new Chat({ messages: msg.text, name: msg.name });
+        chatMsg.save();
+
+        socket.broadcast.emit("received", { text: msg.text, name: chatMsg.get("name"), time: chatMsg.get("timestamp") })
+
+    })
+    socket.on("disconnect", () => {
+        console.log("Disconnected")
+    })
+})
+
 
 const authRoutes = require("./routes/auth/auth")
 
 app.use("/auth", authRoutes)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.get('/health', (req, res) => {
     res.status(200).send({
@@ -22,7 +51,7 @@ app.get('/health', (req, res) => {
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://superuser:clubbruin@cluster0.djc4y.mongodb.net/user?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://superuser:clubbruin@cluster0.pcs6c.mongodb.net/db?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
@@ -36,6 +65,10 @@ const connectToDB = async () => {
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
+})
+
+http.listen(SOCKET_PORT, () => {
+    console.log("connected to port: " + SOCKET_PORT)
 })
 
 connectToDB()
---
+++ models/chat.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+    {
+
+        messages: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+
+    },
+    { timestamps: true }
+);
+
+let Chat;
+module.exports = Chat = mongoose.model("Chat", chatSchema);
