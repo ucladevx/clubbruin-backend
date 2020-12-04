69ec08620cdcf1ba477b4a0f7fb3f0f9c3a6981d
rohanbattula <rohanbattula@gmail.com>
2020-12-03 20:14:40 -0800

    initial fishing schema

---
+++ utils/rooms/fishing-room.js
@@ -0,0 +1,47 @@
+const { Room, Client } = require("colyseus");
+const schema = require('@colyseus/schema');
+const Schema = schema.Schema;
+const MapSchema = schema.MapSchema;
+
+class Fish extends Schema {
+    constructor(){
+        super()
+        this.x = Math.floor(Math.random() * 400);
+        this.y = Math.floor(Math.random() * 400);
+        this.username = "name"
+    }
+}
+schema.defineTypes(Fish, {
+    x: "number",
+    y: "number",
+    username:"string"
+});
+
+class Rod extends Schema {
+    constructor(){
+        super()
+        this.x = Math.floor(Math.random() * 400);
+        this.y = Math.floor(Math.random() * 400);
+        this.username = "name"
+    }
+}
+schema.defineTypes(Fish, {
+    x: "number",
+    y: "number",
+    username:"string"
+});
+
+export class FishingRoom extends Room<GameState> {
+    onCreate(options: any) {
+        this.setState(new GameState())
+    }
+
+    onJoin(client: Client, options: any) {
+    }
+
+    onLeave(client: Client, consented: boolean) {
+    }
+
+    onDispose() {
+    }
+}
