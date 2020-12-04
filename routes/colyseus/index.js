68be6d19e9f623fe8cc4aa06afa5ebcf4eb066f3
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2020-12-03 19:42:57 -0800

    wip

--- routes/colyseus/index.js
+++ routes/colyseus/index.js
@@ -1,13 +1,13 @@
+const path = require('path');
 const router = require('express').Router();
 
 router.get('/map', (req, res) => {
-    res.send("got map");
-    // res.sendFile(path.join(__dirname + '../utils/mock-frontend/map.html'));
-});
-router.get('/game', (req, res) => {
-    // res.sendFile(path.join(__dirname + '../utils/mock-frontend/game.html'));
+    res.sendFile(path.join(__dirname + '/../../utils/mock-frontend/map.html'));
 });
 
+router.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../../utils/mock-frontend/game.html'));
+});
 
 // router.get('/game/:gameId', (req, res) => {
 //     res.sendFile(path.join(__dirname + '../utils/mock-frontend/' + req.params.gameId + '.html'));
